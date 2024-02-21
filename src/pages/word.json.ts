773c6b0d03f5a1bcd9bb9b65f28c79425249c2cf
Vijay Pemmaraju <vijay@grooveapp.com>
2024-02-21 00:06:39 -0800

    Fix build and deploy

--- src/pages/word.json.ts
+++ src/pages/word.json.ts
@@ -18,12 +18,12 @@ let driver: Driver;
   const PASSWORD = process.env.NEO4J_PASSWORD;
 
   try {
-    driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD));
+    driver = neo4j.driver(URI, neo4j.auth.basic(USER, PASSWORD!));
     const serverInfo = await driver.getServerInfo();
     console.log("Connection established");
     console.log(serverInfo);
   } catch (err) {
-    console.log(`Connection error\n${err}\nCause: ${err.cause}`);
+    console.log(`Connection error\n${err}\nCause: ${(err as any).cause}`);
   }
 })();
 

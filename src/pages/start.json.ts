7a26009b5b89c3150e8cfcae3bbff3b1abb411ff
Vijay Pemmaraju <vijay@grooveapp.com>
2024-02-23 12:32:33 -0800

    Fix start.json and fix share dialog

--- src/pages/start.json.ts
+++ src/pages/start.json.ts
@@ -3,6 +3,7 @@ import { z } from "zod";
 import { zodToJsonSchema } from "zod-to-json-schema";
 import driver from "../db/driver";
 
+const cache = new Map<number, string[]>();
 
 // Defining the schema we want our data in
 const actionItemsSchema = z.object({
@@ -17,6 +18,14 @@ export const GET: APIRoute = async ({ url }) => {
   if (!driver) {
     throw new Error("Driver is not initialized");
   }
+
+  const startOfDay = new Date();
+  startOfDay.setHours(0, 0, 0, 0);
+
+  if (cache.has(startOfDay.getDate())) {
+    return new Response(JSON.stringify(cache.get(startOfDay.getDate())));
+  }
+
   // find and delete all nodes that contain a _
   // const result1 = await driver.executeQuery(`
   // MATCH (n:Word)
@@ -24,6 +33,41 @@ export const GET: APIRoute = async ({ url }) => {
   // DETACH DELETE n
   // RETURN n
   // `);
+  const result = await driver.executeQuery(`
+  MATCH (p)
+  RETURN p
+  LIMIT 3000
+`);
 
-return new Response(JSON.stringify(['internet', 'brain']));
+  const words = result.records.map(
+    (record) => record.toObject().p.properties.word
+  );
+
+  let distanceBetweenWords = 0;
+
+  let seed = startOfDay.getDate() % words.length;
+
+  let twoRandomWords = words.slice(seed, (seed + 2) % words.length);
+  while (distanceBetweenWords < 10) {
+    twoRandomWords = words.slice(seed, (seed + 2) % words.length);
+
+    const result = await driver.executeQuery(
+      `
+    MATCH (n:Word {word: $word1}), (p:Word {word: $word2})
+    WHERE n <> p
+    RETURN length(shortestPath((n)-[*]-(p))) AS score
+  `,
+      {
+        word1: twoRandomWords[0],
+        word2: twoRandomWords[1],
+      }
+    );
+    distanceBetweenWords = result.records[0].get("score");
+    seed = (seed + 1) % words.length;
+    console.log(twoRandomWords, distanceBetweenWords.toString());
+  }
+
+  cache.set(startOfDay.getDate(), twoRandomWords);
+
+  return new Response(JSON.stringify(twoRandomWords));
 };

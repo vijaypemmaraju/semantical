301c1c972d1fc7fc766fb431804c25287a4f2e05
Vijay Pemmaraju <vijay@grooveapp.com>
2024-02-23 13:39:38 -0800

    Enable vercel analytics

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import tailwind from "@astrojs/tailwind";
 // https://astro.build/config
 export default defineConfig({
   output: 'server',
-  adapter: vercel(),
+  adapter: vercel({
+    webAnalytics: { enabled: true },
+  }),
   integrations: [tailwind()]
 });
